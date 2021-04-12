49add5ee2fd365521101576ca927c3eea212894c
Artem Vinogradov <artem.vinogradov@hotmail.com>
2021-04-13 00:00:47 +0300

    added react app project folder

---
+++ react-app-currency_hook/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  const [eur, setEur] = useState(0);
+  const [gbp, setGbp] = useState(0);
+
+function calculate(e) {
+  e.preventDefault();
+  const conversion = eur * 0.9;
+  setGbp(conversion);
+}
+
+  return (
+    <div className="container">
+      <form onSubmit={calculate}>
+        <h3>Currency conversion from eur to gbp</h3>
+        <div>
+          <label>Eur</label>
+          <input type="number" step="0.01" value={eur} onChange={e => setEur(e.target.value)} />
+        </div>
+        <div>
+          <label>Gbp</label>
+          <output>{gbp}</output>
+        </div>
+        <div>
+          <button>Calculate</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
